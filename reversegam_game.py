a2b7925a5c10d0600720808145d1137985a6922b
Riddle6 <148794251+Riddle6@users.noreply.github.com>
2025-03-20 12:51:27 +0000

    Work in Progress again

--- reversegam_game.py
+++ reversegam_game.py
@@ -21,3 +21,32 @@ def getNewBoard():
     for i in range(WIDTH):
         board.append([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
     return board
+
+def isValidMove(board, tile, xstart, ystart):
+    # Return False if the player's move on space xstart, ystart is invalid.
+    # If it is a valid move, return a list of spaces that would become the player's if they made a move here.
+    if board[xstart][ystart] != ' ' or not isOnBoard(xstart, ystart):
+        return False
+    
+    if tile == 'X':
+        otherTile = 'O'
+    else:
+        otherTile = 'X'
+
+    tilesToFlip = []
+    for xdirection, ydirection in [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]:
+        x, y = xstart, ystart
+        x += xdirection # First step in the x direction
+        y += ydirection # First step in the y direction
+        while isOnBoard(x, y) and board[x][y] == otherTile:
+            # Keep moving in this x & y direction.
+            x += xdirection
+            y += ydirection
+            if isOnBoard(x, y) and board[x][y] == tile:
+                # There are pieces to flip over. Go in reverse direction until we reach the original space, noting all the tiles along the way.
+                while True:
+                    x -= xdirection
+                    y -= ydirection
+                    if x == xstart and y == ystart:
+                        break
+                    tilesToFlip.append([x, y])
