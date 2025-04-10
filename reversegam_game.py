bfbd1fdc9da14e85a4cfd07e3aba859fe7b4c7c7
Riddle6 <148794251+Riddle6@users.noreply.github.com>
2025-04-10 14:52:11 +0000

    Almost Finished with reversegam_game

--- reversegam_game.py
+++ reversegam_game.py
@@ -50,3 +50,117 @@ def isValidMove(board, tile, xstart, ystart):
                     if x == xstart and y == ystart:
                         break
                     tilesToFlip.append([x, y])
+
+    if len(tilesToFlip) == 0: # If no tiles were flipped, this is not a valid move.
+        return False
+    return tilesToFlip
+
+def isOnBoard(x, y):
+    # Return True if the coordinates are located on the board.
+    return x >= 0 and x <= WIDTH - 1 and y >= 0 and y <= HEIGHT - 1
+
+def getBoardWithValidMoves(board, tile):
+    # Return a new board with periods marking the valid moves the player can make.
+    boardCopy = getBoardCopy(board)
+
+    for x, y in getValidMoves(boardCopy, tile):
+        boardCopy[x][y] = '.'
+    return boardCopy
+
+def getValidMoves(board, tile):
+    # Return a list of [x,y] lists of valid moves for the given player on the given board.
+    validMoves = []
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            if isValidMove(board, tile, x, y) != False:
+                validMoves.append([x, y])
+    return validMoves
+
+def getScoreOfBoard(board):
+    # Determines the score by counting the tiles. Return a dictionary with keys 'X' and 'O'.
+    xscore = 0
+    oscore = 0
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            if board[x][y] == 'X':
+                xscore += 1
+            if board[x][y] == 'O':
+                oscore += 1
+    return {'X':xscore, 'O':oscore}
+
+def enterPlayerTile():
+    # Let the player enter which tile they want to be.
+    # Return a list with the player's tile as the first item and the computer's tile as the second.
+    tile = ''
+    while not (tile =='X' or tile == 'O'):
+        print('Do you want to be X or O?')
+        tile = input().upper()
+
+        # The first element in the list is the player's tile, and the second is the computer's tile.
+        if tile == 'X':
+            return ['X', 'O']
+        else:
+            return ['O', 'X']
+
+def whoGoesFirst():
+    # Randomly choose who goes first.
+    if random.randint(0, 1) == 0:
+        return 'computer'
+    else:
+        return 'player'
+    
+def makeMove(board, tile, xstart, ystart):
+    # Place the tile on the board at xstart, ystart and flip any of the opponent's pieces
+    # Return False if this is an invalid move; True if it is valid.
+    tilesToFlip = isValidMove(board, tile, xstart, ystart)
+
+    if tilesToFlip == False:
+        return False
+    
+    board[xstart][ystart] = tile
+    for x, y in tilesToFlip:
+        board[x][y] = tile
+    return True
+
+def getBoardCopy(board):
+    # Make a duplicate of the board list and return it.
+    boardCopy = getNewBoard()
+
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            boardCopy[x][y] = board[x][y]
+
+    return boardCopy
+
+def isOnCorner(x, y):
+    # Return True if the position is in one of the four corners.
+    return (x == 0 or x == WIDTH - 1) and (y == 0 or y == HEIGHT - 1)
+
+def getPlayerMove(board, playerTile):
+    # Let the player enter their move.
+    # Return the move as [x, y] (or return the strings 'hints' or 'quit')
+    DIGITS1TO8 = '1 2 3 4 5 6 7 8'.split()
+    while True:
+        print('Enter your move, "quit" to end the game, or "hints" to toggle hints.')
+        move = input().lower()
+        if move == 'quit' or move == 'hints':
+            return move
+        
+        if len(move) == 2 and move[0] in DIGITS1TO8 and move[1] in DIGITS1TO8:
+            x = int(move[0]) - 1
+            y = int(move[1]) - 1
+            if isValidMove(board, playerTile, x, y) == False:
+                continue
+            else:
+                break
+        else:
+            print('That is not a valid move. Enter the column (1-8) and then the row (1-8).')
+            print('For example, 81 will move on the top-right corner.')
+
+    return [x, y]
+
+def getComputerMove(board, computerTile):
+    # Given a board and the computer's tile, determine where to
+    # move and return that move as an [x, y] list.
+    possibleMoves = getValidMoves(board, computerTile)
+    random.shuffle(possibleMoves) # Randomize the order of the moves.
